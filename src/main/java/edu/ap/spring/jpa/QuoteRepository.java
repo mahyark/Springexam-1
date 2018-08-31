db6b2638b68d2f99d47b5a832e2fd2d1c0cf82f5
Mahyar Moghimi Kian <mahyar.kian@gmail.com>
2018-08-31 13:19:27 +0200

    added methods to repository

--- src/main/java/edu/ap/spring/jpa/QuoteRepository.java
+++ src/main/java/edu/ap/spring/jpa/QuoteRepository.java
@@ -1,9 +1,17 @@
 package edu.ap.spring.jpa;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuoteRepository extends CrudRepository<Quote, Long> {
+	
+	// find all Quotes
+	List<Quote> findAll();
+	
+	// find by tagname
+	List<Quote> findByTagName(String tagName);
 }
 
