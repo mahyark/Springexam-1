243f2a30a8c961b732b264d4000b2e537ec2d0f7
Mahyar Moghimi Kian <mahyar.kian@gmail.com>
2018-08-31 13:18:51 +0200

    Added columns to Quote class

--- src/main/java/edu/ap/spring/jpa/Quote.java
+++ src/main/java/edu/ap/spring/jpa/Quote.java
@@ -1,13 +1,48 @@
 package edu.ap.spring.jpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Quote {
+
+	@Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String quoteDescription;
+    
+    @Column
+    private String tagName;
+
+	public Quote() {}
 	
-	   @Id
-	   @GeneratedValue
-	   private Long id;
+    public Quote(String quoteDesc, String tag) {
+	    this.quoteDescription = quoteDesc;
+	    this.tagName = tag;
+    }
+
+	public String getQuoteDescription() {
+		return quoteDescription;
+	}
+
+	public void setQuoteDescription(String quoteDescription) {
+		this.quoteDescription = quoteDescription;
+	}
+
+	public String getTagName() {
+		return tagName;
+	}
+
+	public void setTagName(String tagName) {
+		this.tagName = quoteDescription;
+	}
+	   
+    @Override
+    public String toString() {
+        return "Quote{Description: " + this.quoteDescription + ", Tag: " + this.tagName + "}";
+    } 
 }
